5ef1c75abe04c1586742cbf0aacc6013f7dc9cca
XxGamallxX <gamalmicheal12@gmail.com>
2025-05-02 14:40:43 +0300

    input filter

--- desktop-real-time-chat-app/src/components/Chat/ChatLayout.jsx
+++ desktop-real-time-chat-app/src/components/Chat/ChatLayout.jsx
@@ -4,11 +4,16 @@ import MessageRoom from './MessageRoom/MessageRoom';
 import MessageInput from './MessageInput/MessageInput';
 import './ChatLayout.css';
 import ChatList from './chatList';
-import SearchBar from './searchbar/Searchbar';
+import SearchBar from './searchbar/ChatSearchInput';
 import { useState , useRef } from 'react';
+import ChatRightSidebar from './Sidebar/ChatRightSideBar';
+
 const ChatLayout = () => {
   const [message, setMessage] = useState(null); 
+
+
   return (         
+    // left side bar
     <div className="chat-layout-container">
       <div className="chat-main-card">
         <div className="chat-layout">
@@ -23,13 +28,14 @@ const ChatLayout = () => {
           </div>
           {/* right-sidebar */}
           <div className="right-sidebar flex flex-col h-full overflow-hidden">
-            <h2 className="logo-text p1">Messages</h2>
+            {/* <h2 className="logo-text p1">Messages</h2>
             <SearchBar />
             <div className="flex">
               <img src="/images/image (7).png" className="pleft" />
               <h4 className="logo-text">All Chat</h4>
             </div>
-            <ChatList setMessage={setMessage} />
+            <ChatList setMessage={setMessage} /> */}
+            <ChatRightSidebar/>
           </div>
         </div>
       </div>
